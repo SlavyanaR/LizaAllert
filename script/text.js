cdb17481d10d9417dee8e8b37fa109a3d801dbf9
SlavyanaR <95542891+SlavyanaR@users.noreply.github.com>
2022-04-10 17:56:14 +0300

    правка стилей

--- script/text.js
+++ script/text.js
@@ -7,3 +7,17 @@ indexListItemNodes.forEach((indexListItemNode) => {
     indexListItemNode.classList.toggle('index-list__item_visible');
   });
 });
+const goToCourseButton = document.querySelector('#goToCourse');
+const goToVebinarButton = document.querySelector('#goToVebinar');
+
+function goToCourseButtonHandler() {
+    document.location.href = './course.html';
+  }
+
+goToCourseButton.addEventListener('click', goToCourseButtonHandler);
+
+function goToVebinarButtonHandler() {
+    document.location.href = './vebinar.html';
+  }
+
+goToVebinarButton.addEventListener('click', goToVebinarButtonHandler);
--- script/vebinar.js
+++ script/vebinar.js
@@ -7,3 +7,18 @@ indexListItemNodes.forEach((indexListItemNode) => {
     indexListItemNode.classList.toggle('index-list__item_visible');
   });
 });
+
+const goToVideoButton = document.querySelector('#goToVideo');
+const goToTextButton = document.querySelector('#goToText');
+
+function goToVideoButtonHandler() {
+    document.location.href = './video.html';
+  }
+
+goToVideoButton.addEventListener('click', goToVideoButtonHandler);
+
+function goToTextButtonHandler() {
+    document.location.href = './text.html';
+  }
+
+goToTextButton.addEventListener('click', goToTextButtonHandler);
